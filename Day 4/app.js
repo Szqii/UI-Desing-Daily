62e9921e62d1bbdf287c140c8d1eb30089cd8e7e
Szqii <szqii.altn@gmail.com>
2020-12-14 12:13:59 +0300

    Inital commit

--- Day 4/app.js
+++ Day 4/app.js
@@ -1,6 +1,20 @@
+
 $(document).ready(function () {
-    $('#title').click(function () {
-        $('#title').removeClass('active');
-        $(this).addClass('active');
+    $(".title").click(function () {
+        $(".title").removeClass("active");
+        $(this).addClass("active");
+        console.log("Sa")
     });
-});	
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      wrapperState : true,
+    },
+    methods: {
+        change : function(){
+            this.wrapperState = this.wrapperState == true ? false : true
+        },
+    },
+  })
